e66fe9787e193b9c39c9945a8b32c4c854a837c7
ArthurYu <598228123@qq.com>
2019-04-03 11:01:12 +0800

    Update ILSItem.cs

--- Assets/Sctipts/ObjInfoSave/ILSItem.cs
+++ Assets/Sctipts/ObjInfoSave/ILSItem.cs
@@ -9,7 +9,7 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-[RequireComponent(typeof(ObjID))]
+[RequireComponent(typeof(ObjID)), DisallowMultipleComponent]
 public class ILSItem : MonoBehaviour {
 
     [EnumFlagsAttribute]
